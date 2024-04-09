7f8371cebd2d7adc1f89b217c3db698cf492501a
xmilla99 <kbja22@aau.student.dk>
2024-04-09 12:58:36 +0200

    Starter ASTTests i ASTTest.java

---
+++ tests/ASTTest.java
@@ -0,0 +1,8 @@
+import gen.ExprLexer;
+import gen.ExprParser;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import org.junit.Assert;
+import org.junit.Test;
+public class ASTTest {
+}
