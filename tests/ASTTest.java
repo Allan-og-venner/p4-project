df20df36f3c5935c3042541be1d9824bde78a0f3
xmilla99 <kbja22@student.aau.dk>
2024-04-15 15:40:00 +0200

    Prøver lige no'et

--- tests/ASTTest.java
+++ tests/ASTTest.java
@@ -1,8 +1,35 @@
 import gen.ExprLexer;
 import gen.ExprParser;
+import nodes.BlockNode;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+
 public class ASTTest {
+
+    //set up data to be used for tests
+    String userDir = System.getProperty("user.dir");
+    CharStream in;
+
+    {
+        try {
+            in = CharStreams.fromFileName(userDir+"/src/code.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    ExprLexer lexer = new ExprLexer(in);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    ExprParser parser = new ExprParser(tokens);
+    ExprParser.ProgContext tree = parser.prog();
+    BlockNode ast = new BuildASTVisitor().visitProg(tree);
+    // EvaluateExpressionVisitor value = new EvaluateExpressionVisitor();
+    // System.out.println(value.visit(ast));
+
+
+
 }
