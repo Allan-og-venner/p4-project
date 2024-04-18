ba30fd763a7a5eb75f9d9debc7c59b722cae6c91
xmilla99 <kbja22@aau.student.dk>
2024-04-18 12:34:58 +0200

    LexerTest.java tilføjet ny test

--- tests/LexerTest.java
+++ tests/LexerTest.java
@@ -22,7 +22,7 @@ public class LexerTest {
             System.out.println(t);
         }
 
-        assertEquals(4, allTokens.size()); // Expecting three tokens: INT, '+', INT
+        assertEquals(4, allTokens.size()); // Expecting four tokens: INT, '+', INT, EOF
 
         // Check first token is an integer
         assertEquals(ExprLexer.NUMERAL, allTokens.get(0).getType());
@@ -35,5 +35,27 @@ public class LexerTest {
         // Check third token is an integer
         assertEquals(ExprLexer.NUMERAL, allTokens.get(2).getType());
         assertEquals("4", allTokens.get(2).getText());
+    }
+
+    @Test
+    public void testCharToken() {
+        String expression = "'c'";
+        ExprLexer lexer = new ExprLexer(CharStreams.fromString(expression));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        tokens.fill(); // Load all tokens to inspect them
+
+        List<Token> allTokens = tokens.getTokens();
+
+        //print tokens for debugging
+        for (Token t : allTokens) {
+            System.out.println(t);
+        }
+
+        assertEquals(2, allTokens.size()); // Expecting two tokens: CHAR, EOF
+
+        // Check token is a string
+        assertEquals(ExprLexer.CHAR, allTokens.get(0).getType());
+        assertEquals("'c'", allTokens.get(0).getText());
+
     }
 }
