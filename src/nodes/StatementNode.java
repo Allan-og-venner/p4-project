4eed283c6c54a4e6d064a755999ffd05639d35b9
SimonSkywalker <sthordal@gmail.com>
2024-05-17 09:43:07 +0200

    changed StatementNode

--- src/nodes/StatementNode.java
+++ src/nodes/StatementNode.java
@@ -1,20 +1,5 @@
 package nodes;
 
 public abstract class StatementNode extends BlockNode {
-    private StatementNode left;
-    private StatementNode right;
 
-    public StatementNode getLeft() {
-        return left;
-    }
-    public void setLeft(StatementNode left) {
-        this.left = left;
-    }
-
-    public StatementNode getRight() {
-        return right;
-    }
-    public void setRight(StatementNode right) {
-        this.right = right;
-    }
 }
